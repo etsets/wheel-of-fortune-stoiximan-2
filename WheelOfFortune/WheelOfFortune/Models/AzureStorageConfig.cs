aee8f6e367522a52861a54c0fab0c52b710c8f94
pbariamoglou <pan_mpa@hotmail.com>
2017-10-22 14:30:54 +0300

    second_try_register_faceapi_upload_different_forms

---
+++ WheelOfFortune/WheelOfFortune/Models/AzureStorageConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WheelOfFortune.Models
+{
+    public class AzureStorageConfig
+    {
+        //public string AccountName { get; set; }
+        public string AccountName = "gypweufs01";
+        //public string AccountKey { get; set; }
+        public string AccountKey = "BVcWDWEvOZedWGNV0AZd0UpdJ8HkQFEaE3JBvC8C2DCC3byn4HIrCuQ9ZNDlB3WzCMha4OQuKLwHkQOoN5Ui1A==";
+        public string QueueName { get; set; }
+        //public string ImageContainer { get; set; }
+        public string ImageContainer = "faceapi";
+        //public string ThumbnailContainer { get; set; }
+        public string ThumbnailContainer = "faceapi-thumb";
+
+    }
+}
