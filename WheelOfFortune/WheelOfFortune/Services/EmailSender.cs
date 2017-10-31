76902dda81fa1439f3ff214a944f6d2d799dbdde
Elis Mucelli <elis.mucelli@gmail.com>
2017-10-31 14:15:09 +0200

    Λειτουργεί όταν ο χρήστης έχει κάνει confirm το email του

--- WheelOfFortune/WheelOfFortune/Services/EmailSender.cs
+++ WheelOfFortune/WheelOfFortune/Services/EmailSender.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace WheelOfFortune.Services
@@ -9,9 +11,46 @@ namespace WheelOfFortune.Services
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
+
+        private readonly string  host;
+        private readonly string username;
+        private readonly string password;
+
+        private SmtpClient smtp;
+
+        public EmailSender()
+        {
+            host = "e-creativity.gr";
+            username = "info@e-creativity.gr";
+            password = "e5632411256E$";
+
+            smtp = new SmtpClient();
+            smtp.UseDefaultCredentials = false;
+            smtp.Host = host;
+            smtp.Credentials = new NetworkCredential(username, password);
+
+        }
+
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            return Task.CompletedTask;
+            MailMessage mailMessage = new MailMessage();
+
+            mailMessage.From = new MailAddress(this.username);
+            mailMessage.Subject = subject;
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Body = message;
+            mailMessage.To.Add(email);
+
+            try
+            {
+                smtp.Send(mailMessage);
+                return Task.CompletedTask;
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Could not send email");
+            }
+            
         }
     }
 }
