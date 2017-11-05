48f5d0fc75311d07a8ae7b2a49f7d455598df715
pbariamoglou <pan_mpa@hotmail.com>
2017-11-05 14:15:20 +0200

    gamer_history_without_visual
    Successfully get gamer history with UserId property.
    Without visual effects.

--- WheelOfFortune/WheelOfFortune/Controllers/PlayController.cs
+++ WheelOfFortune/WheelOfFortune/Controllers/PlayController.cs
@@ -5,24 +5,47 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using WheelOfFortune.Data;
+using WheelOfFortune.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
 
 namespace WheelOfFortune.Controllers
 {
     public class PlayController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-          [Authorize]
+        public PlayController(
+            ApplicationDbContext context,
+          UserManager<ApplicationUser> userManager,
+          SignInManager<ApplicationUser> signInManager
+            )
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [Authorize]
           [ResponseCache(NoStore = true, Duration = 0)]
           public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult History()
+        public async Task<IActionResult> History()
         {
-            return View();
-        }
+            var user = await _userManager.GetUserAsync(User);
 
+            var entries = await _context.HistoryEntries
+                .Where(h => h.CreatedBy.Equals(user.Id))
+                .ToListAsync();
+            
+            return View(entries);
+        }
 
     }
 }
---
+++ WheelOfFortune/WheelOfFortune/Models/Deposit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WheelOfFortune.Models
+{
+    public class DepositEntry
+    {
+        public int HistoryEntryId { get; set; }
+        [ForeignKey("HistoryEntryId")]
+        public HistoryEntry BelongsToHistoryEntry { get; set; }
+
+        public int VoucherId { get; set; }
+        [ForeignKey("VoucherId")]
+        public Voucher BelongsToVoucherEntry { get; set; }
+    }
+}
