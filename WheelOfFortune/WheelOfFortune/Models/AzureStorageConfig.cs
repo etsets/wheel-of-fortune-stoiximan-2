64c6d8999933e97f8d5bcf4c1521c3cad2d61d00
pbariamoglou <pan_mpa@hotmail.com>
2017-11-12 16:03:24 +0200

    fixed_hidden_keys

--- WheelOfFortune/WheelOfFortune/Models/AzureStorageConfig.cs
+++ WheelOfFortune/WheelOfFortune/Models/AzureStorageConfig.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace WheelOfFortune.Models
 {
     public class AzureStorageConfig
     {
-        public string AccountName = "gypweufs01";
-        public string AccountKey = "BVcWDWEvOZedWGNV0AZd0UpdJ8HkQFEaE3JBvC8C2DCC3byn4HIrCuQ9ZNDlB3WzCMha4OQuKLwHkQOoN5Ui1A==";
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
         public string QueueName { get; set; }
         public string ImageContainer = "faceapi";
         public string ThumbnailContainer = "faceapi-thumb";
+
     }
 }
