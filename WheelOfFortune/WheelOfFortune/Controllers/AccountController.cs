a74abceb28dde677b17e27c6cc285c9725a8f898
etsets <etsets84@gmail.com>
2017-11-18 17:59:10 +0200

    Enable check for validated mail

--- WheelOfFortune/WheelOfFortune/Controllers/AccountController.cs
+++ WheelOfFortune/WheelOfFortune/Controllers/AccountController.cs
@@ -82,7 +82,7 @@ namespace WheelOfFortune.Controllers
             {
 
                     // Require the user to have a confirmed email before they can log on.
-                    /*var user = await _userManager.FindByEmailAsync(model.Email);
+                    var user = await _userManager.FindByEmailAsync(model.Email);
                     if (user != null)
                     {
                          if (!await _userManager.IsEmailConfirmedAsync(user))
@@ -91,7 +91,7 @@ namespace WheelOfFortune.Controllers
                                             "You must have a confirmed email to log in.");
                               return View(model);
                          }
-                    }*/
+                    }
 
 
 
